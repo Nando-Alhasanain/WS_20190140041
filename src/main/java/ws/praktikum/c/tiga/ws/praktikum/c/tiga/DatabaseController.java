131c7e044e26c65242558a5c39e133ecaa569588
Nando Alhasanain <Nandoalhasanain@gmail.com>
2021-12-22 21:35:01 +0700

    commit ws

---
+++ src/main/java/ws/praktikum/c/tiga/ws/praktikum/c/tiga/DatabaseController.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ws.praktikum.c.tiga.ws.praktikum.c.tiga;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ *
+ * @author Nando
+ */
+@Controller
+public class DatabaseController {
+    
+    @CrossOrigin
+    @RequestMapping(value="/datamhsJSON", produces={MediaType.APPLICATION_JSON_VALUE})
+    
+    @ResponseBody
+    public List<Datamahasiswa> getDatamhs(){
+        
+        List<Datamahasiswa> datamahasiswa = new ArrayList<>();
+        
+        DatamahasiswaJpaController controller = new DatamahasiswaJpaController();
+        
+        try {
+            datamahasiswa = controller.findDatamahasiswaEntities();
+        } catch (Exception e){}
+        
+        return datamahasiswa;
+    }
+
+}
